4446aa50e79f2dd0d5842e1f6b5d12b693a9ee7b
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-12-05 14:23:04 +0700

    Config navigation

--- App.tsx
+++ App.tsx
@@ -3,18 +3,22 @@ import { Text, View } from "react-native";
 import DetailsScreen from "./components/review/details";
 import AboutScreen from "./components/review/about";
 import HomeScreen from "./components/review/home";
-
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { OPENSANT } from "./utils/const";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 SplashScreen.preventAutoHideAsync();
+
+const Stack = createStackNavigator();
 
 function App() {
   const [loaded, error] = useFonts({
     [OPENSANT]: require("./assets/fonts/OpenSans-Regular.ttf"),
   });
+
   useEffect(() => {
     if (loaded || error) {
       SplashScreen.hideAsync();
@@ -23,12 +27,13 @@ function App() {
   if (!loaded && !error) {
     return null;
   }
+
   return (
-    <View>
-      <HomeScreen />
-      <AboutScreen />
-      <DetailsScreen />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
